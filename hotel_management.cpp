24b0c7b2ee7b8718fe337b6d77daf87de7a214e2
Shivank Pathak <shivankpathak357@gmail.com>
2017-11-10 09:07:40 +0530

    FIX MINOR BUGS

--- hotel_management.cpp
+++ hotel_management.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctime>
+#include <termios.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -20,7 +23,7 @@ using namespace std;
 class hotel
 {
     long d,m,y,nom,n,r,rno;
-    char name[40],address[80],a[20],pno[20];
+    char name[40],address[80],type[20],pno[20];
 
 public:
     
@@ -46,19 +49,31 @@ void welcome()
     cp(MAG"WELCOME"RESET);
     gotoxy(20,12);
     cp(YEL "INTERNATIONAL INSTITUTE\n\t\t   OF INFORMATION TECHNOLOGY\n\t\t\tBHUBANESHWAR"RESET);
-    gotoxy(28,15);
+    gotoxy(20,16);
+    cp(YEL"OBJECT ORIENTED PROGRAMMING\n\t\t\t   PROJECT"RESET);
+    gotoxy(28,18);
     cp(MAG"PROJECT-"RESET);
-    gotoxy(22,16);
+    gotoxy(22,18);
     cp(YEL"HOTEL MANAGEMENT PROGRAM"RESET);
     gotoxy(14,20);
     cp(MAG"NAME-"RESET);
-    cp(YEL "Shivank Pathak"RESET);
-    gotoxy(14,22);
-    cp(MAG"YEAR-"RESET);
-    cp(YEL "2nd"RESET);
+    cp(YEL "Shivank Pathak(b416045)"RESET);
+    gotoxy(19,21);
+    cp(YEL"Vijendra Ku. Sharma(B516052)"RESET);
+    gotoxy(19,22);
+    cp(YEL"Rishav Ku. Jha(b316039)"RESET);
     gotoxy(14,24);
+    cp(MAG"SEM-"RESET);
+    cp(YEL "3rd"RESET);
+    gotoxy(14,26);
     cp(MAG"BRANCH-"RESET);
     cp(YEL "INFORMATION TECHNOLOGY"RESET);
+    gotoxy(14,29);
+    cp(MAG"ORDINATOR-"RESET);
+    cp(YEL"Mr.Suraj Sharma"RESET);
+    gotoxy(14,31);
+    cp(MAG"CO-ORDINATOR-"RESET);
+    cp(YEL"Mr.Rathin Chandra Shit"RESET);   
     gotoxy(14,28);
     for(i=0;i<40;++i)
     {gotoxy(a+i,b);
@@ -76,7 +91,7 @@ void welcome()
     {gotoxy(a,b+i);
     printf(RED"*"RESET);
     }
-    gotoxy(25,32);
+    gotoxy(25,35);
     cp(CYN"PRESS ENTER TO START"RESET);
     getchar();
     system("clear");
@@ -87,7 +102,7 @@ void hotel::checkin()
 {
     int check;
     FILE *f;
-    f=fopen("//home//shivank//Desktop//hotel.dat","ab+");
+    f=fopen("hotel.dat","ab+");
     system("clear");
     gotoxy(5,5);
     cp(BLU"ENTER NAME"RESET);
@@ -124,29 +139,34 @@ void hotel::checkin()
     scanf("%d",&check);
 
     switch(check)
-    {case 1:strcpy(a,"DELUXE ROOM");
+    {case 1:strcpy(type,"DELUXE ROOM");
             break;
-    case 2:strcpy(a,"SUITE");
+    case 2:strcpy(type,"SUITE");
             break;
-    case 3:strcpy(a,"PARLOUR");
+    case 3:strcpy(type,"PARLOUR");
             break;
-    case 4:strcpy(a,"JUNIOUR SUITE");
+    case 4:strcpy(type,"JUNIOUR SUITE");
             break;
-    case 5:strcpy(a,"EFFICIENCY ROOM");
+    case 5:strcpy(type,"EFFICIENCY ROOM");
             break;
-    case 6:strcpy(a,"HOSPITALITY ROOM");
+    case 6:strcpy(type,"HOSPITALITY ROOM");
             break;
     default:cp("WRONG INPUT");
     }
 
+
+    time_t t = time(0);
+    struct tm * now = localtime( & t );
+    //cout<<"d"<<now->tm_mday<<"m"<<now->tm_mon<<"y"<<now->tm_year<<endl;
+    y=(now->tm_year + 1900);
+    m=(now->tm_mon + 1);
+    d=now->tm_mday;
+
+
     fwrite(&h,sizeof(h),1,f);
- /* struct date d;
-    getdate(&d);
-    e.d=d.da_day;
-    e.m=d.da_mon;
-    e.y=d.da_year; */
+
     gotoxy(10,30);
-    cp("THANK YOU");
+    cp(CYN"THANK YOU"RESET);
     getchar();
     getchar();
     fclose(f);
@@ -156,7 +176,7 @@ void hotel::checkin()
 void hotel::search()
 {
     FILE *f;
-    f=fopen("//home//shivank//Desktop//hotel.dat","rb+");
+    f=fopen("hotel.dat","rb+");
     fread(&h,sizeof(h),1,f);
     char na[20];
     int a=0;
@@ -174,28 +194,29 @@ void hotel::search()
         if(strcmp(na,name)==0)
         {
             gotoxy(5,8);
-            cp(GRN"NAME="RESET);
+            cp(GRN"NAME: "RESET);
             gotoxy(12,8);
             printf("%s",name);
             gotoxy(5,10);
-            cp(GRN"NO. OF MEMBERS="RESET);
-            gotoxy(19,10);
+            cp(GRN"NO. OF MEMBERS: "RESET);
+            gotoxy(24,10);
             printf("%ld",nom);
             gotoxy(5,12);
-            cp(GRN"PHONE NUMBER="RESET);
+            cp(GRN"PHONE NUMBER: "RESET);
             gotoxy(20,12);
             printf("%s\n",pno);
-            /*gotoxy(5,14);
-            cp("DATE OF CHECKIN=");
-            cout<<e.d<<"/"<<e.m<<"/"<<e.y<<endl;*/
             gotoxy(5,14);
-            cp(GRN"NO. OF DAYS TO STAY:"RESET);
+            cp(GRN"DATE OF CHECKIN: "RESET);
             gotoxy(25,14);
-            printf("%ld\n",n);
+            cout<<d<<"/"<<m<<"/"<<y<<endl;
             gotoxy(5,16);
-            cp(GRN"TYPE OF ROOM IN WHICH THE PERSON STAYED IS:"RESET);
-            gotoxy(40,16);
-            printf("%s\n",a);
+            cp(GRN"NO. OF DAYS TO STAY: "RESET);
+            gotoxy(27,16);
+            printf("%ld\n",n);
+            gotoxy(5,18);
+            cp(GRN"TYPE OF ROOM IN WHICH THE PERSON STAYED IS: "RESET);
+            gotoxy(50,18);
+            printf("%s\n",type);
             a=1;
         }
     fread(&h,sizeof(h),1,f);
@@ -221,17 +242,19 @@ void hotel::checkout()
     gets(na);
     system("clear");
     gotoxy(10,4);
-    cp("BILL\n");
+    cp(RED"BILL\n"RESET);
+    gotoxy(5,5);
+    cp(RED"-------------"RESET);
     gotoxy(4,6);
-    cp(YEL"NAME:\n"RESET);
+    cp(YEL"NAME:"RESET);
     printf(MAG"%s",na);
     gotoxy(4,8);
-    cp(YEL"ROOM TYPE:\n"RESET);
-    f=fopen("//home//shivank//Desktop//hotel.dat","rb+");
+    cp(YEL"ROOM TYPE:"RESET);
+    f=fopen("hotel.dat","rb+");
     FILE *f1;
-    f1=fopen("//home//shivank//Desktop//tempr.dat","wb+");
+    f1=fopen("tempr.dat","wb+");
     FILE *f2;
-    f2=fopen("//home//shivank//Desktop//cht.dat","ab+");
+    f2=fopen("cht.dat","ab+");
     fread(&h,sizeof(h),1,f);
     while(1)
     {
@@ -241,32 +264,32 @@ void hotel::checkout()
         {
             fwrite(&h,sizeof(h),1,f2);
             gotoxy(15,8);
-            printf(MAG"%s"RESET,a);
+            printf(MAG"%s"RESET,type);
             gotoxy(4,10);
-            cp(YEL"TOTAL AMOUNT TO BE PAID:\n"RESET);
-            if(strcmp(a,"DELUXE ROOM"))
+            cp(YEL"TOTAL AMOUNT TO BE PAID:"RESET);
+            if(strcmp(type,"DELUXE ROOM"))
             {
-                printf("%ld",50000*n);
+                printf(MAG"%ld"RESET,50000*n);
             }
-            else if(strcmp(a,"SUITE"))
+            else if(strcmp(type,"SUITE"))
             {
-                printf("%ld",45000*n);
+                printf(MAG"%ld"RESET,45000*n);
             }
-            else if(strcmp(a,"PARLOUR"))
+            else if(strcmp(type,"PARLOUR"))
             {
-                printf("%ld",40000*n);
+                printf(MAG"%ld"RESET,40000*n);
             }
-            else if(strcmp(a,"JUNIOUR SUITE"))
+            else if(strcmp(type,"JUNIOUR SUITE"))
             {
-                printf("%ld",35000*n);
+                printf(MAG"%ld"RESET,35000*n);
             }
-            else if(strcmp(a,"EFFICIENCY ROOM"))
+            else if(strcmp(type,"EFFICIENCY ROOM"))
             {
-                printf("%ld",30000*n);
+                printf(MAG"%ld"RESET,30000*n);
             }
-            else if(strcmp(a,"HOSPITALITY ROOM"))
+            else if(strcmp(type,"HOSPITALITY ROOM"))
             {
-                printf("%ld",25000*n);
+                printf(MAG"%ld"RESET,25000*n);
             }
         }
         fwrite(&h,sizeof(h),1,f1);
@@ -277,7 +300,7 @@ void hotel::checkout()
         fclose(f2);
         fclose(f);
         gotoxy(10,15);
-        cp("THANK YOU");
+        cp(CYN"THANK YOU"RESET);
         getchar();
 }
 
@@ -336,69 +359,70 @@ int main()
     
     int y,k,a=29,b=14,i;
     welcome();
-    do
-    {
-        system("clear");
 
-        for(i=0;i<16;++i)
+        do
         {
-            gotoxy(a+i,b);
-            cp(CYN"*");
-        }
-        for(i=0;i<8;++i)
-        {
-            gotoxy(a,b+i);
-            cp("*");
-        }
-        for(i=0;i<8;++i)
-        {
-            gotoxy(a+16,b+i);
-            cp("*");
-        }
-        for(i=0;i<16;++i)
-        {
-            gotoxy(a+i,b+8);
-            cp("*");
-        }
-        gotoxy(30,15);
-        cp(YEL"MAIN MENU:"RESET);
-        gotoxy(30,16);
-        //tc(CYAN);
-        cp(CYN"1.CHECKIN");
-        gotoxy(30,17);
-        cp("2.CHECKOUT");
-        gotoxy(30,18);
-        cp("3.SEARCH");
-        gotoxy(30,19);
-        cp("4.FOOD");
-        gotoxy(30,20);
-        cp("5.EXIT");
-        printf("\n");
-        gotoxy(15,25);
-        cp(GRN"ENTER YOUR CHOICE"RESET);
-        gotoxy(35,25);
-        scanf("%d",&y);
-        switch(y)
-        {
-            case 1:h.checkin();
-            break;
-            case 2:h.checkout();
-            break;
-            case 3:h.search();
-            break;
-            case 4:h.food();
-            break;
-            case 5:system("clear");
-            gotoxy(25,14);
-            cp(RED"ARE YOU SURE YOU WANT TO EXIT"RESET);
-            gotoxy(28,15);
-            cp(YEL"1.YES       2.NO"RESET);
-            int x;
-            scanf("%d",&x);
-            if(x!=1)
-            y++;
-        }
-    }while(y!=5);
+            system("clear");
+
+            for(i=0;i<16;++i)
+            {
+                gotoxy(a+i,b);
+                cp(CYN"*");
+            }
+            for(i=0;i<8;++i)
+            {
+                gotoxy(a,b+i);
+                cp("*");
+            }
+            for(i=0;i<8;++i)
+            {
+                gotoxy(a+16,b+i);
+                cp("*");
+            }
+            for(i=0;i<16;++i)
+            {
+                gotoxy(a+i,b+8);
+                cp("*");
+            }
+
+            gotoxy(30,15);
+            cp(YEL"MAIN MENU:"RESET);
+            gotoxy(30,16);
+            cp(CYN"1.CHECKIN");
+            gotoxy(30,17);
+            cp("2.CHECKOUT");
+            gotoxy(30,18);
+            cp("3.SEARCH");
+            gotoxy(30,19);
+            cp("4.FOOD");
+            gotoxy(30,20);
+            cp("5.EXIT");
+            printf("\n");
+            gotoxy(15,25);
+            cp(GRN"ENTER YOUR CHOICE"RESET);
+            gotoxy(35,25);
+            scanf("%d",&y);
+            switch(y)
+            {
+                case 1:h.checkin();
+                break;
+                case 2:h.checkout();
+                break;
+                case 3:h.search();
+                break;
+                case 4:h.food();
+                break;
+                case 5:system("clear");
+                gotoxy(25,14);
+                cp(RED"ARE YOU SURE YOU WANT TO EXIT"RESET);
+                gotoxy(28,15);
+                cp(YEL"1.YES       2.NO"RESET);
+                int x;
+                scanf("%d",&x);
+                if(x!=1)
+                y++;
+            }
+        }while(y!=5);
 
     
     return(0);
